4c68bf3f25cd016d1b335cd9c8f86de95b9d4b6e
Illia <koshkin061969@gmail.com>
2023-07-05 09:09:41 +0200

    Version 0.0.1

--- ToDoie/ToDoie/Program.cs
+++ ToDoie/ToDoie/Program.cs
@@ -1,13 +1,46 @@
-﻿using System;
-using Discord;
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 
-namespace ToDoie
+namespace BasicBot
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+
+        public async Task MainAsync()
         {
-            Console.WriteLine("Hello World!");
+            _client = new DiscordSocketClient();
+
+            _client.Log += Log;
+            _client.MessageReceived += ClientOnMessageReceived;
+
+            var token = "";
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+        private static Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        private static Task ClientOnMessageReceived(SocketMessage arg)
+        {
+            if (arg.Content.StartsWith("!helloworld"))
+            {
+                arg.Channel.SendMessageAsync($"User '{arg.Author.Username}' successfully ran helloworld!");
+            }
+            return Task.CompletedTask;
         }
     }
 }
