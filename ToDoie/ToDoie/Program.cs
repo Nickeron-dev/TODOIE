fa8f09143f54815ec21b1bcb35b4053fcb21e211
Illia <koshkin061969@gmail.com>
2023-07-05 09:15:50 +0200

    Version 0.02
    Implement menu

--- ToDoie/ToDoie/Program.cs
+++ ToDoie/ToDoie/Program.cs
@@ -8,39 +8,105 @@ namespace BasicBot
 {
     class Program
     {
-        public static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+        // Non-static readonly fields can only be assigned in a constructor.
+        // If you want to assign it elsewhere, consider removing the readonly keyword.
+        private readonly DiscordSocketClient _client;
 
-        private DiscordSocketClient _client;
+        // Discord.Net heavily utilizes TAP for async, so we create
+        // an asynchronous context from the beginning.
+        static void Main(string[] args)
+            => new Program()
+                .MainAsync()
+                .GetAwaiter()
+                .GetResult();
+
+        public Program()
+        {
+            // Config used by DiscordSocketClient
+            // Define intents for the client
+            var config = new DiscordSocketConfig
+            {
+                GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent
+            };
+
+            // It is recommended to Dispose of a client when you are finished
+            // using it, at the end of your app's lifetime.
+            _client = new DiscordSocketClient(config);
+
+            // Subscribing to client events, so that we may receive them whenever they're invoked.
+            _client.Log += LogAsync;
+            _client.Ready += ReadyAsync;
+            _client.MessageReceived += MessageReceivedAsync;
+            _client.InteractionCreated += InteractionCreatedAsync;
+        }
 
         public async Task MainAsync()
         {
-            _client = new DiscordSocketClient();
+            // Tokens should be considered secret data, and never hard-coded.
+            await _client.LoginAsync(TokenType.Bot, ("token"));
+            // Different approaches to making your token a secret is by putting them in local .json, .yaml, .xml or .txt files, then reading them on startup.
 
-            _client.Log += Log;
-            _client.MessageReceived += ClientOnMessageReceived;
-
-            var token = "";
-
-            await _client.LoginAsync(TokenType.Bot, token);
             await _client.StartAsync();
 
-            // Block this task until the program is closed.
-            await Task.Delay(-1);
+            // Block the program until it is closed.
+            await Task.Delay(Timeout.Infinite);
         }
 
-        private static Task Log(LogMessage msg)
+        private Task LogAsync(LogMessage log)
         {
-            Console.WriteLine(msg.ToString());
+            Console.WriteLine(log.ToString());
             return Task.CompletedTask;
         }
 
-        private static Task ClientOnMessageReceived(SocketMessage arg)
+        // The Ready event indicates that the client has opened a
+        // connection and it is now safe to access the cache.
+        private Task ReadyAsync()
         {
-            if (arg.Content.StartsWith("!helloworld"))
+            Console.WriteLine($"{_client.CurrentUser} is connected!");
+
+            return Task.CompletedTask;
+        }
+
+        // This is not the recommended way to write a bot - consider
+        // reading over the Commands Framework sample.
+        private async Task MessageReceivedAsync(SocketMessage message)
+        {
+            // The bot should never respond to itself.
+            if (message.Author.Id == _client.CurrentUser.Id)
+                return;
+
+
+            if (message.Content == "!ping")
             {
-                arg.Channel.SendMessageAsync($"User '{arg.Author.Username}' successfully ran helloworld!");
+                // Create a new ComponentBuilder, in which dropdowns & buttons can be created.
+                var cb = new ComponentBuilder()
+                    .WithButton("Click me!", "unique-id", ButtonStyle.Primary);
+
+                // Send a message with content 'pong', including a button.
+                // This button needs to be build by calling .Build() before being passed into the call.
+                await message.Channel.SendMessageAsync("pong!", components: cb.Build());
             }
-            return Task.CompletedTask;
+            if (message.Content == "!start")
+            {
+
+                message.Channel.SendMessageAsync("Hi, ich bin dein ToDoie-Bot und helfe dir gerne deinen Alltag organisierter zu gestalten. Dies kann ich für dich tun:");
+                message.Channel.SendMessageAsync("-task erstellen \n-task entfernen");
+            }
+        }
+
+        private async Task InteractionCreatedAsync(SocketInteraction interaction)
+        {
+            // safety-casting is the best way to prevent something being cast from being null.
+            // If this check does not pass, it could not be cast to said type.
+            if (interaction is SocketMessageComponent component)
+            {
+                // Check for the ID created in the button mentioned above.
+                if (component.Data.CustomId == "unique-id")
+                    await interaction.RespondAsync("Thank you for clicking my button!");
+
+                else
+                    Console.WriteLine("An ID has been received that has no handler!");
+            }
         }
     }
 }
